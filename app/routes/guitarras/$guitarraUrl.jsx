80c98f3c0fb52f0cb0001828c39fc53010187455
Armando Ruiz <armando.ruiz@industrie.co>
2022-12-06 10:05:56 +1100

    211. mostrando la informacion de la guitarra y agredando css

--- app/routes/guitarras/$guitarraUrl.jsx
+++ app/routes/guitarras/$guitarraUrl.jsx
@@ -1,5 +1,15 @@
 import { useLoaderData } from '@remix-run/react'
 import { getGuitarra } from '~/models/guitarras.server'
+import styles from '~/styles/guitarras.css'
+
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
 
 export async function loader({params}) {
   //usamos el loader para traer la info de la guitarra
@@ -16,11 +26,17 @@ export async function loader({params}) {
 
 const Guitarra = () => {
   const guitarra = useLoaderData() // usamos el useLoaderData para traerl los datos del servidor al front end
-  console.log(guitarra)
+  // console.log(guitarra)
+  const { nombre, descripcion, imagen, precio } = guitarra.data[0].attributes
 
   return (
-    <div>
-      $guitarraUrl
+    <div className='guitarra'>
+      <img className='imagen' src={imagen.data.attributes.url} alt={`Imagen de la guitarra ${nombre}`} />
+      <div className='contenido'>
+        <h3>{nombre}</h3>
+        <p className='texto'>{descripcion}</p>
+        <p className='precio'>${precio}</p>
+      </div>
     </div>
   );
 };
