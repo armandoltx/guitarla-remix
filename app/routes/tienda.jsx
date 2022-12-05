db5dc23cb73642bca1fc7096dd9f8f95f5e8b1c8
Armando Ruiz <armando.ruiz@industrie.co>
2022-12-05 15:54:02 +1100

    204. Consumiendo el backend de strapi con loader

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,4 +1,14 @@
-
+export async function loader() {
+  // en remix no hay q asociarlo, solo exportandolo sabe q es un loader
+  // loader se utiliza cuadno el componente carga
+  // Action es cuando envias datos desde un formulario
+  console.log('desde loader') // se ve en el servidor
+  const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`);
+  const resultado = await respuesta.json()
+  // console.log(resultado)
+  // console.log(process.env.API_URL)
+  return {}
+}
 const Tienda = () => {
   return (
     <div>
