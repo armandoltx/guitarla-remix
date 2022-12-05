e9d3b74bd94c05616875f7e229c0bae4b6452c8b
Armando Ruiz <armando.ruiz@industrie.co>
2022-12-06 09:59:27 +1100

    210. Filtrando las guitarras usando el url, pasandolas desde strapi(back) al front

--- app/models/guitarras.server.js
+++ app/models/guitarras.server.js
@@ -4,3 +4,10 @@ export async function getGuitarras() {
   const resultado = await respuesta.json()
   return resultado
 }
+
+export async function getGuitarra(url) {
+  // para filtrat por el atributo q sea, se pasa la palabra filters y un array y dentor del array se agrega el atributo por el q se filtra
+  const respuesta = await fetch(`${process.env.API_URL}/guitarras?filters[url]=${url}&populate=imagen`);
+  const resultado = await respuesta.json()
+  return resultado
+}
--- app/routes/guitarras/$guitarraUrl.jsx
+++ app/routes/guitarras/$guitarraUrl.jsx
@@ -1,12 +1,23 @@
+import { useLoaderData } from '@remix-run/react'
+import { getGuitarra } from '~/models/guitarras.server'
 
 export async function loader({params}) {
+  //usamos el loader para traer la info de la guitarra
   // console.log(request)
-  console.log(params)
+  // console.log(params)
 
-  return { guitarraUrl } = params
+  const { guitarraUrl } = params
+
+  const guitarra = await getGuitarra(guitarraUrl)
+  // console.log(guitarra)
+
+  return guitarra
 }
 
 const Guitarra = () => {
+  const guitarra = useLoaderData() // usamos el useLoaderData para traerl los datos del servidor al front end
+  console.log(guitarra)
+
   return (
     <div>
       $guitarraUrl
