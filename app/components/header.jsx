569798e8cf8ab3e989be17d346860b83a064a05a
Armando Ruiz <armando.ruiz@industrie.co>
2022-12-05 14:59:54 +1100

    199. Agregando la clase active al menu link q este activo

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,25 +1,34 @@
-import { Link } from '@remix-run/react'
+import { Link, useLocation } from '@remix-run/react'
 import logo from '../../public/img/logo.svg'
 
 const Header = () => {
+  const location = useLocation()
+
   return (
     <header className="header">
       <div className="contenedor barra">
-        <Link to="/">
-          <img src={logo} alt="Logotipo" className="logo" />
+        <Link
+          to="/"
+          className={location.pathname === "/" ? 'active' : ''}
+          >
+            <img src={logo} alt="Logotipo" className="logo" />
         </Link>
         <nav className="navegacion">
           <Link
             to= "/"
+            className={location.pathname === "/" ? 'active' : ''}
           >Inicio</Link>
           <Link
             to= "/nosotros"
+            className={location.pathname === "/nosotros" ? 'active' : ''}
           >Nosotros</Link>
           <Link
             to= "/tienda"
+            className={location.pathname === "/tienda" ? 'active' : ''}
           >Tienda</Link>
           <Link
             to= "/blog"
+            className={location.pathname === "/blog" ? 'active' : ''}
           >Blog</Link>
         </nav>
       </div>
