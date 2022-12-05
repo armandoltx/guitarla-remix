d9683f0a0f0015be46b3c635b6c28ba2cd970373
Armando Ruiz <armando.ruiz@industrie.co>
2022-12-06 08:52:50 +1100

    207.  Mostrando la informacion de cada guitarra

--- app/components/guitarra.jsx
+++ app/components/guitarra.jsx
@@ -1,9 +1,18 @@
-
+import { Link } from '@remix-run/react'
 const Guitarra = ({guitarra}) => {
-  console.log(guitarra)
+  // console.log(guitarra)
+  const { descripcion, imagen, precio, url, nombre } = guitarra
+  // console.log(imagen.data.attributes.formats.medium.url) // hemos hecho console.log de cadfa uno de ellos para llegar a lo q queriamos
   return (
-    <div>
-      guitarra
+    <div className="guitarra">
+      <img src={imagen.data.attributes.formats.medium.url} alt={`Imagen guitarra ${nombre}`} />
+      <div className="contenido">
+        <h3>{nombre}</h3>
+        <p className="descripcion">{descripcion}</p>
+        <p className="precio">${precio}</p>
+
+        <Link className='enlace' to={`/guitarras/${url}`}>Ver Producto</Link>
+      </div>
     </div>
   );
 };
