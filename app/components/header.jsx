7d6346be26395d3e4df07d20092fa7275a1e8573
Armando Ruiz <armando.ruiz@industrie.co>
2022-12-05 14:51:53 +1100

    197. Agregando el logotipo al header

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,10 +1,13 @@
 import { Link } from '@remix-run/react'
+import logo from '../../public/img/logo.svg'
 
 const Header = () => {
   return (
     <header className="header">
       <div className="contenedor barra">
-        <div className="logo"></div>
+        <Link to="/">
+          <img src={logo} alt="Logotipo" className="logo" />
+        </Link>
         <nav className="navegacion">
           <Link
             to= "/"
