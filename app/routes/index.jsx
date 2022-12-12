86dfc960cd49da7bf7142cd274c048328713d129
Armando Ruiz <armando.ruiz@industrie.co>
2022-12-12 16:16:57 +1100

    221. Agregar el curso a la pagina inicial, creando el componente y agregando el css

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,10 +1,13 @@
 import { useLoaderData } from '@remix-run/react'
 import { getGuitarras } from '~/models/guitarras.server'
 import { getPosts } from '~/models/posts.server'
+import { getCurso } from '~/models/curso.server'
 import ListadoGuitarras from '~/components/listado-guitarras'
 import ListadoPosts from '~/components/listado-posts'
+import Curso from '~/components/curso'
 import stylesGuitarras from '~/styles/guitarras.css'
 import stylesPosts from '~/styles/blog.css'
+import stylesCurso from '~/styles/curso.css'
 
 
 export function meta() {}
@@ -18,6 +21,10 @@ export function links() {
     {
       rel: 'stylesheet',
       href: stylesPosts
+    },
+    {
+      rel: 'stylesheet',
+      href: stylesCurso
     }
   ]
 }
@@ -26,9 +33,10 @@ export async function loader() {
   // como queremos hacer dos llamadas independientes, una a guitarras y otra a posts, usamos promisee all,
   // pq si hacemos una llamada y lugo la otra, la 2 no se realiza hasta q no termina la 1, y en este caso no necesitamos eso
 
-  const [guitarras, posts] = await Promise.all([
+  const [guitarras, posts, curso] = await Promise.all([
     getGuitarras(),
-    getPosts()
+    getPosts(),
+    getCurso()
   ])
 
   // console.log(guitarras)
@@ -38,34 +46,40 @@ export async function loader() {
   // console.log(guitarras)
   // const posts = await getPosts()
   // console.log(posts)
+  // console.log("here here here")
+  console.log(curso)
 
   return {
     guitarras: guitarras.data,
-    posts: posts.data
+    posts: posts.data,
+    curso: curso.data
   }
 }
 
-const Index = () => {
-  const { guitarras, posts } = useLoaderData()
+function Index() {
+
+  const {guitarras, posts, curso } = useLoaderData();
   // console.log(guitarras)
   // console.log(posts)
-
-
   return (
     <>
-      <main className="contenedor">
+      <main className='contenedor'>
         <ListadoGuitarras
-          guitarras={guitarras}
+            guitarras={guitarras}
         />
       </main>
 
-      <section className="contenedor">
-        <ListadoPosts
-        posts={posts}
+      <Curso
+        curso={curso.attributes}
       />
-      <section/>
-    </>
-  );
-};
 
-export default Index;
+      <section className='contenedor'>
+        <ListadoPosts
+          posts={posts}
+        />
+      </section>
+    </>
+  )
+}
+
+export default Index
