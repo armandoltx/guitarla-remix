151f8da83806fd5c9d1f026fae64e5dce3e9b434
Armando Ruiz <armando.ruiz@industrie.co>
2022-12-16 13:10:08 +1100

    239. Agregando y usando localStorage

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -36,8 +36,8 @@ const Carrito = () => {
       <div className="contenido">
         <div className="carrito">
           <h2>Articulos</h2>
-          {carrito.length === 0 ? 'Carrito Vacio' : (
-            carrito.map( producto => (
+          {carrito?.length === 0 ? 'Carrito Vacio' : (
+            carrito?.map( producto => (
               <div className='producto' key={producto.id}>
                 <div>
                   <img src={producto.imagen} alt={`Imagen del producto {producto.nombre}`} />
